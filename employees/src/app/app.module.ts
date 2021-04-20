0ffcf6ccb73ae1d0ae9ba74a5ff525d1c839137a
Sergio Tribaldos <sergio.tribaldos@dxc.com>
2021-04-20 17:45:51 +0200

    feat: create home component

--- employees/src/app/app.module.ts
+++ employees/src/app/app.module.ts
@@ -8,7 +8,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
