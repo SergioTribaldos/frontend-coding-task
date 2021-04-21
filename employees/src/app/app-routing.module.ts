77def9af2c82abdd1586fc0a100b264bacc3fd3a
Sergio Tribaldos <sergio.tribaldos@dxc.com>
2021-04-21 12:25:55 +0200

    feat: redirect unknown routes to home

--- employees/src/app/app-routing.module.ts
+++ employees/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  }
+  },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
