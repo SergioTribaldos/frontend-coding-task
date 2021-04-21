9bda23bbfc920b68fe575ca8c00378a1fe8c3ec1
Sergio Tribaldos <sergio.tribaldos@dxc.com>
2021-04-21 20:30:49 +0200

    feat: create get employees in firebase service

--- employees/src/app/core/services/firebase.service.ts
+++ employees/src/app/core/services/firebase.service.ts
@@ -1,23 +1,32 @@
 import {Injectable} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Employee} from '../models/employees.model';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
+  private readonly COLLECTION = 'employees';
 
   constructor(private db: AngularFirestore) {
+  }
+
+  getEmployees(): Observable<Employee[]> {
+    return this.db.collection(this.COLLECTION).get().pipe(
+      map((data) => data.docs.map((doc) => doc.data() as Employee))
+    );
   }
 
   addEmployee(employee: Employee) {
   }
 
-  removeEmployee(employee:Employee){
+  removeEmployee(employee: Employee) {
 
   }
 
-  updateEmployee(employee:Employee){
+  updateEmployee(employee: Employee) {
 
   }
 }
