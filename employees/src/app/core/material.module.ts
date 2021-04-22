bee0efdc21d58dbc79b5c34f245f0f8541981214
Sergio Tribaldos <sergio.tribaldos@dxc.com>
2021-04-22 16:33:32 +0200

    feat: add loading spinner

--- employees/src/app/core/material.module.ts
+++ employees/src/app/core/material.module.ts
@@ -4,12 +4,35 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatCardModule, MatTableModule],
-  exports: [MatToolbarModule, MatButtonModule, MatCardModule, MatTableModule]
+  imports: [CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatProgressSpinnerModule],
+
+  exports: [MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatProgressSpinnerModule]
 })
 export class MaterialModule {
 }
