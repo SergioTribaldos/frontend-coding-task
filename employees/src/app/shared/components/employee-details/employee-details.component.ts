bc4a99c32b981ce8ddb155dea675dc3371d1346d
Sergio Tribaldos <sergio.tribaldos@dxc.com>
2021-04-23 11:18:24 +0200

    feat: implement logic to display the dynamic form

--- employees/src/app/shared/components/employee-details/employee-details.component.ts
+++ employees/src/app/shared/components/employee-details/employee-details.component.ts
@@ -1,4 +1,11 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+
+import {Subject} from 'rxjs';
+
+import {FormTransformService} from '../../../core/services/form-transform.service';
+import {debounceTime, takeUntil} from 'rxjs/operators';
+import {DynamicFormConfig} from '../../../core/models/dynamic-form.model';
 
 @Component({
   selector: 'app-employee-details',
@@ -6,12 +13,31 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
   styleUrls: ['./employee-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EmployeeDetailsComponent implements OnInit {
+export class EmployeeDetailsComponent implements OnInit, OnDestroy {
+  @Input() formConfig: DynamicFormConfig[];
 
-  constructor() {
+  @Output() formChanged = new EventEmitter<FormGroup>();
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  formGroup: FormGroup;
+
+  constructor(private formTransformService: FormTransformService) {
   }
 
   ngOnInit(): void {
+    this.formGroup = this.formTransformService.createFormGroup(this.formConfig);
+    this.formChanged.emit(this.formGroup);
+    this.formGroup.valueChanges.pipe(
+      debounceTime(500),
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      this.formChanged.emit(this.formGroup);
+    });
+
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
