d5351e3cc5ab1c5a39f883b964eb57ae477816c7
Sergio Tribaldos <sergio.tribaldos@dxc.com>
2021-04-23 19:39:38 +0200

    refactor: move default config to a separate constant

---
+++ employees/src/app/employee-create/constants/form-config.constants.ts
@@ -0,0 +1,37 @@
+import {DynamicFormConfig} from '../../core/models/dynamic-form.model';
+
+export const DEFAULT_FORM_CONFIG: DynamicFormConfig[] = [
+  {
+    type: 'input',
+    displayName: 'Name',
+    controlName: 'name',
+    value: '',
+    editable: true,
+    errorMessage: 'Name is required'
+  },
+  {
+    type: 'input',
+    displayName: 'Surname',
+    controlName: 'surname',
+    value: '',
+    editable: true,
+    errorMessage: 'Surname is required'
+  },
+  {
+    type: 'datepicker',
+    displayName: 'Date of Birth',
+    controlName: 'dateOfBirth',
+    value: '',
+    editable: true,
+    errorMessage: 'Date of birth is required'
+  },
+  {
+    type: 'select',
+    displayName: 'Select position',
+    controlName: 'workPosition',
+    value: '',
+    editable: true,
+    errorMessage: 'Position is required',
+    options: []
+  }
+];
