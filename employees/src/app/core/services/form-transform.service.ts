7cca6028a6e5124f5b220847e6e9bc2207fb0241
Sergio Tribaldos <sergio.tribaldos@dxc.com>
2021-04-24 08:42:41 +0200

    feat: add method to merge employee with dynamic form config

--- employees/src/app/core/services/form-transform.service.ts
+++ employees/src/app/core/services/form-transform.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 import {DynamicFormConfig} from '../models/dynamic-form.model';
+import {Employee} from '../models/employees.model';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +24,13 @@ export class FormTransformService {
     }, {} as DynamicFormConfig);
 
     return this.formBuilder.group(form);
+  }
+
+  mergeEmployeeWithDynamicFormConfig(employee: Employee, dynamicFormConfig: DynamicFormConfig[]): DynamicFormConfig[] {
+    return dynamicFormConfig.map((formConfig) => {
+      const matchingValue = Object.entries(employee).find(([key, _]) => key === formConfig.controlName);
+      const [value, ...rest] = (matchingValue as [string, any]).reverse();
+      return {...formConfig, value};
+    });
   }
 }
