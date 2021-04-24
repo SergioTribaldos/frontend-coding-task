97036ea5c1db829c326d9ed71dd58a8a68f08dd7
Sergio Tribaldos <sergio.tribaldos@dxc.com>
2021-04-24 15:25:53 +0200

    test: create tests for employee-list

--- employees/src/app/shared/components/employee-list/employee-list.component.spec.ts
+++ employees/src/app/shared/components/employee-list/employee-list.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { EmployeeListComponent } from './employee-list.component';
+import {EmployeeListComponent} from './employee-list.component';
+import {MatTableModule} from '@angular/material/table';
+import {EMPLOYEES_STUB} from '../../stubs/stubs';
 
 describe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
@@ -8,9 +10,10 @@ describe('EmployeeListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EmployeeListComponent ]
+      imports: [MatTableModule],
+      declarations: [EmployeeListComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +25,25 @@ describe('EmployeeListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should show two rows is there is two employees', () => {
+    fixture = TestBed.createComponent(EmployeeListComponent);
+    component = fixture.componentInstance;
+    component.employees = EMPLOYEES_STUB;
+    fixture.detectChanges();
+    const rowHtmlElements: NodeList = fixture.debugElement.nativeElement.querySelectorAll('tbody tr');
+    expect(rowHtmlElements.length).toBe(2);
+  });
+
+  it('Should show a "no results card" when there are no employees', () => {
+    fixture = TestBed.createComponent(EmployeeListComponent);
+    component = fixture.componentInstance;
+    component.employees = [];
+    fixture.detectChanges();
+    const noResultsCard: NodeList = fixture.debugElement.nativeElement.querySelectorAll('.no-results');
+    expect(noResultsCard.length).toBe(1);
+
+  });
+
+
 });
