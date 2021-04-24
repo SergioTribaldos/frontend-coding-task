9f37b458cdadf8bcbc3f8c28115964a6f64275df
Sergio Tribaldos <sergio.tribaldos@dxc.com>
2021-04-24 15:24:14 +0200

    test: create stub file

---
+++ employees/src/app/shared/stubs/stubs.ts
@@ -0,0 +1,18 @@
+import {Employee} from '../../core/models/employees.model';
+
+export const EMPLOYEES_STUB: Employee[] = [
+  {
+    id: 'hjf88hf3',
+    name: 'sergio',
+    surname: 'tribaldos',
+    workPosition: 'React',
+    dateOfBirth: new Date()
+  },
+  {
+    id: 'hjf88hf3',
+    name: 'John',
+    surname: 'Connor',
+    workPosition: 'Angular',
+    dateOfBirth: new Date()
+  }
+];
